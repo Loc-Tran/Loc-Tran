1601c33a50cce2aea41dcb7d797e6eefc2491c0c
1991loctran@gmail.com <1991loctran@gmail.com>
2019-09-05 11:27:29 +1000

    has a navbar now

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Single page personal website.
 Aims are to learn React and showcase my work.
+ReactModules not commited because ignored on purpose. Use NPM to reconstruct.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/style.css';
 import { Button } from './button.js';
 
-function App() {
-  return (
-    <div className="App">
-        <div>
-          <Button name='About Me'/>
-          <Button name='CV'/>
-          <Button name='Contact Me'/>
-        </div>
-    </div>
-  );
+const title = "Loc Tran - Software Developer";
+const cvSharedLink = "https://www.dropbox.com/s/k6xvgkrpvnaigf4/Resume - Loc Tran - Personal Website.pdf?dl=0";
+const email = "1991loctran@gmail.com";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    document.title = title;
+  }
+
+  render() {
+    return (
+      <body id="top">
+     		<header id="header">
+     		    <h1><a href="#">Loc Tran</a></h1>
+				    <nav className="nav">
+					    <ul>
+						    <li><a href="index.html">Home</a></li>
+						    <li><a href={cvSharedLink} target="popup">CV</a></li>
+						    <li><a href={"mailto:" + email}>Contact Me</a></li>
+					    </ul>
+				    </nav>
+				</header>
+      </body>
+    );
+  }
 }
 
 export default App;
