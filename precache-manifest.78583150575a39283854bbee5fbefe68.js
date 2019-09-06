4b49de094e450d869e454d023813214ef8a5758c
1991loctran@gmail.com <1991loctran@gmail.com>
2019-09-06 18:43:07 +1000

    Updates

---
+++ precache-manifest.78583150575a39283854bbee5fbefe68.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f8456fff6a37a4f7586cf5ef34f88e",
+    "url": "/Loc-Tran/index.html"
+  },
+  {
+    "revision": "9b4217dcf253f617b3b5",
+    "url": "/Loc-Tran/static/css/main.f0e6676b.chunk.css"
+  },
+  {
+    "revision": "763f994f0b56124c8be2",
+    "url": "/Loc-Tran/static/js/2.61d06600.chunk.js"
+  },
+  {
+    "revision": "9b4217dcf253f617b3b5",
+    "url": "/Loc-Tran/static/js/main.62da3e55.chunk.js"
+  },
+  {
+    "revision": "3d55c98a3501b653f2f6",
+    "url": "/Loc-Tran/static/js/runtime~main.b130bf58.js"
+  },
+  {
+    "revision": "65bcbc899f379216109acd0b6c494618",
+    "url": "/Loc-Tran/static/media/fontawesome-webfont.65bcbc89.svg"
+  },
+  {
+    "revision": "7149833697a959306ec3012a8588dcfa",
+    "url": "/Loc-Tran/static/media/fontawesome-webfont.71498336.eot"
+  },
+  {
+    "revision": "c4668ed2440df82d3fd2f8be9d31d07d",
+    "url": "/Loc-Tran/static/media/fontawesome-webfont.c4668ed2.ttf"
+  },
+  {
+    "revision": "d95d6f5d5ab7cfefd09651800b69bd54",
+    "url": "/Loc-Tran/static/media/fontawesome-webfont.d95d6f5d.woff"
+  },
+  {
+    "revision": "78f15aaec6e40c8a86c5382c08c69dcd",
+    "url": "/Loc-Tran/static/media/pic02.78f15aae.jpg"
+  },
+  {
+    "revision": "0de1ea0f6e5e349c2fb0e10d2731af83",
+    "url": "/Loc-Tran/static/media/space.0de1ea0f.png"
+  }
+]);
